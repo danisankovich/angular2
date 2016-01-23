b7eb7ddbb1557f2dbf80cf8ee52c17df2ae51572
Michael <misankovich@gmail.com>
2016-01-23 02:35:03 -0500

    finished functionality done

--- app/app.component.js
+++ app/app.component.js
@@ -1,9 +1,22 @@
 (function(app) {
   app.AppComponent = ng.core.Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template:
+      '<h1>{{title}}</h1>' +
+      '<h1>{{y}}</h1>' +
+        '<p><button class="btn btn-primary" role="button" (click)="up()">+</button>' +
+        '<button class="btn btn-primary" role="button" (click)="down()">-</button></p>'
   })
   .Class({
-    constructor: function() {}
+    constructor: function() {
+      this.y = 0
+      this.title = 'Forrent Incrementations'
+    },
+    down() {
+      this.y -= 1
+    },
+    up() {
+      this.y += 1
+    }
   })
 })(window.app || (window.app = {}))
